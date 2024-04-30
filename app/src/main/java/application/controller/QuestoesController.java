c203ea83d1c738249e8d5f5b6ced3af719bf8c93
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2024-04-30 18:30:50 +0000

    Concluido a criação do controlador Questoes e seus testes em http

--- app/src/main/java/application/controller/QuestoesController.java
+++ app/src/main/java/application/controller/QuestoesController.java
@@ -1,11 +1,18 @@
 package application.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import application.model.Questao;
 import application.repository.QuestaoRepository;
@@ -21,8 +28,45 @@ public class QuestoesController {
         return questoesRepo.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Questao getOne(@PathVariable long id) {
+        Optional<Questao> result = questoesRepo.findById(id);
+        if(result.isEmpty()) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Questão não encontrada"
+            );
+        }
+        return result.get();
+    }
+
     @PostMapping
     public Questao post(@RequestBody Questao questao) {
         return questoesRepo.save(questao);
+    }
+
+    @PutMapping("/{id}")
+    private Questao put(@RequestBody Questao questao, @PathVariable long id) {
+        Optional<Questao> result = questoesRepo.findById(id);
+
+        if(result.isEmpty()) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Questão não encontrada"
+            );
+        }
+
+        result.get().setEnunciado(questao.getEnunciado());
+        result.get().setCategoria(questao.getCategoria());
+        return questoesRepo.save(result.get());
+    }
+
+    @DeleteMapping("/{id}")
+    private void delete(@PathVariable long id) {
+        if(questoesRepo.existsById(id)) {
+            questoesRepo.deleteById(id);
+        } else {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Questão não encontrada"
+            );
+        }
     }
 }
